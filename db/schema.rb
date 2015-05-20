5d34a6b73174fbf33382ef325e2f1ad0d9460537
Hannah Stephenson <littlehannah96@gmail.com>
2015-05-20 12:45:50 +0100

    Added one-to-many relationships and set edit/delete permissions.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518193733) do
+ActiveRecord::Schema.define(version: 20150520114339) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
